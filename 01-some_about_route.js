4648b413a3a0cd016c0fec8d126f5b5da59aa1c4
liangrz <shasrm@2980.com>
2017-04-14 13:26:34 +0800

    Update 01-some_about_route.js

--- 01-some_about_route.js
+++ 01-some_about_route.js
@@ -1,9 +1,6 @@
-//1、同一个路径建立两个同类请求的路由，如果没有next()，只会实现第一个
-//2、res.send()输出页面只能在同一类的请求输出一次，二次请求会报错//Can't set headers after they are sent.
-
---
-//例如访问localhost:3000/test
-
+#1、同一个路径建立两个同类请求的路由，如果没有next()，只会实现第一个
+#2、res.send()输出页面只能在同一类的请求输出一次，二次请求会报错//Can't set headers after they are sent.
+#例如访问localhost:3000/test
 
 //----------/router/test.js
 var express = require('express');
@@ -33,12 +30,12 @@ app.listen(3000);
 
 //观看后台，只输出
 
-//first ask
+#first ask
 
 //把//next()的备注斜杠去掉，再次刷新页面，观看后台，能看见
 
-//first ask
-//second ask
+#first ask
+#second ask
 //但是同时还有报错
-//Can't set headers after they are sent.
+#Can't set headers after they are sent.
 //字面意思，页面内容已经发送过后无法再发送
